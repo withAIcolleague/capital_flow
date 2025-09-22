07cc7ffa98d14b485b711ca693590a37cc3815dc
withAIcolleague <endurancedy@gmail.com>
2025-09-23 08:08:04 +0900

    dependencie add

--- .next/static/Cl0VMFNtY0vEEBowLvQ2y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/562-62a00708fa68ed2c.js","static/chunks/pages/index-6c9c7d8445287e6c.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
